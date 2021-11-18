8f041c287933636ee133bffa1e4a807af8c1cbbf
tgiannotta <t.giannotta92@gmail.com>
2021-11-18 18:49:32 +0100

    End milestone 3

--- milestone 2/js/script.js
+++ milestone 2/js/script.js
@@ -47,15 +47,47 @@ for(let i = 0; i < items.length; i++) {
     </div>
     `;
     imageContainer.innerHTML += immagineAttuale;
-    //dichiaro il Thumb attuale
+    //dichiaro il Thumb attuale e lo popolo
     let thumbAttuale = `
     <div class="single-image">
         <img src="${thisPath}" alt="${i}">
     </div>  
     `
     thumb.innerHTML += thumbAttuale;
-    //Popolo il Thumb
-
 }
+//inseriamo gli elementi active
+let activeImage = 0;
+const allImages = document.getElementsByClassName('single-big-image');
+allImages[activeImage].classList.add('active');
+
+const nextArrow = document.querySelector('.arrow-top');
+nextArrow.addEventListener('click', function() {
+    // Rimuovere active all'immagine corrente
+    allImages[activeImage].classList.remove('active');
 
 
+    if( activeImage < items.length - 1 ) {
+        activeImage++;
+    } else {
+        activeImage = 0;
+    }
+
+    // assegnare all'immagine col nouvo indice la classe active
+    allImages[activeImage].classList.add('active');
+});
+const prevArrow = document.querySelector('.arrow-bot');
+
+prevArrow.addEventListener('click', function() {
+    // Rimuovere active all'immagine corrente
+    allImages[activeImage].classList.remove('active');
+
+    // se l'immagine è l'ultima ricomincio da zero
+    if ( activeImage > 0 ) {
+        activeImage--;
+    } else {
+        activeImage = items.length - 1;
+    }
+
+    // assegnare all'immagine col nouvo indice la classe active
+    allImages[activeImage].classList.add('active');
+});
